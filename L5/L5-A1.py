99474033ea64095905dbc1a7ac7657562a98f2e2
hajwahusham <hajwahusham@gmail.com>
2025-10-29 17:30:47 +0500

    activiteS

---
+++ L5/L5-A1.py
@@ -0,0 +1,15 @@
+# swapping two numbers
+def swap(a,b):
+    a = a^b
+    b = a^b
+    a = a^b
+    print("after swapping a=", a, "and b =", b)
+
+def swap2(a,b):
+    a = (a&b) + (a|b)
+    b = a + (~b) +1
+    a = a + (~b) +1
+    print("after swapping a=", a, "and b =", b)
+
+swap(10,20)
+swap2(10,20)
